6183c7181c6df50fdca30a0f194443d4f3266a36
jchadwick <jchadwick@firstam.com>
2022-10-06 16:34:38 -0700

    adding aws adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,11 @@
-import adapter from '@sveltejs/adapter-auto';
+import { adapter } from 'sveltekit-adapter-aws';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true,
-	}),
-
-	kit: {
-		adapter: adapter()
-	}
+export default {
+  preprocess: preprocess(),
+  kit: {
+    adapter: adapter({
+      autoDeploy: true,
+    }),
+  },
 };
-
-export default config;
